1cc064974d03e5aad0c9ccb530d10160f54f993e
Alexandre Siljegovic <alexandresiljegovic22@gmail.com>
2024-02-09 13:33:12 +0100

    axios test register

--- stackoverflow-react/src/axios/axios.js
+++ stackoverflow-react/src/axios/axios.js
@@ -1,27 +1,69 @@
-import axios from 'axios';
+import axios from "axios";
 
-
-axios.defaults.baseURL = 'http://127.0.0.1:8000/';
-
+const baseURL = "http://127.0.0.1:8000/";
+const token = "";
+const api = axios.create({
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `${token}`,
+  },
+});
 // function to make API requests
-export const fetchData = async (url) => {
-  try {
-    const response = await axios.get(url);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    throw error;
-  }
-};
+api
+  .get("/users")
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
+export function registerUser(data) {
+  api
+    .post("/register/", data)
+    .then((response) => {
+      console.log("Register response:", response.data);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+}
+api
+  .post("/login/", {
+    username: "username",
+    password: "password",
+  })
+  .then((response) => {
+    console.log("Login response:", response.data);
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
 
-// Example usage in other components
-// const fetchDataExample = async () => {
-//   try {
-//     const data = await fetchData('/users');
-//     console.log('Fetched data:', data);
-//   } catch (error) {
-//     console.error('Error:', error);
-//   }
-// };
+// api
+//   .post("/register/", {
+//     username: "username",
+//     password: "password",
+//     email: "email",
+//     first_name: "first_name",
+//     last_name: "last_name",
+//   })
+//   .then((response) => {
+//     console.log("Register response:", response.data);
+//   })
+//   .catch((error) => {
+//     console.error("Error fetching data:", error);
+//   });
 
-// fetchDataExample();
+api
+  .get("/logout/")
+  .then((response) => {
+    console.log("Logout response:", response.data);
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
+
+api.get("/user/").then((response) => {
+  console.log("User response:", response.data);
+});
--- stackoverflow-react/src/components/Signup.jsx
+++ stackoverflow-react/src/components/Signup.jsx
@@ -1,60 +1,70 @@
 import React from "react";
-
+import { registerUser } from "../axios/axios";
 function SignUpForm() {
+	function handleSubmit(event) {
+		event.preventDefault();
+		registerUser({
+			username: event.target.username.value,
+			password: event.target.password.value,
+			email: event.target.email.value,
+			first_name: event.target.first_name.value,
+			last_name: event.target.last_name.value,
+		});
+	}
 	return (
 		<section className="bg-gray-50 dark:bg-gray-900 w-full">
 			<div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
 				<div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
 					<div className="p-6 space-y-4 md:space-y-6 sm:p-8">
 						<h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Create your account</h1>
-						<form className="space-y-4 md:space-y-6" action="#">
+						<form className="space-y-4 md:space-y-6" action="#" onSubmit={handleSubmit}>
 							<div>
-								<label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+								<label htmlFor="first name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
 									First-Name
 								</label>
 								<input
-									type="email"
-									name="email"
-									id="email"
+									type="test"
+									name="first_name"
+									id="first_name"
 									className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 									placeholder="First Name"
 									required=""
 								/>
 							</div>
 							<div>
-								<label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+								<label htmlFor="last name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
 									Last Name
 								</label>
 								<input
-									type="password"
-									name="password"
-									id="password"
+									type="text"
+									name="last_name"
+									id="last_name"
 									placeholder="Last Name"
 									className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 									required=""
 								/>
 							</div>
 							<div>
-								<label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+								<label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
 									Username
 								</label>
 								<input
-									type="password"
-									name="password"
-									id="password"
+									type="username"
+									name="username"
+									id="username"
 									placeholder="Username"
 									className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 									required=""
 								/>
 							</div>
 							<div>
-								<label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+								<label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
 									Email
 								</label>
 								<input
-									type="password"
-									name="password"
-									id="password"
+									type="email"
+									name="email"
+									id="email"
 									placeholder="name@company.com"
 									className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 									required=""

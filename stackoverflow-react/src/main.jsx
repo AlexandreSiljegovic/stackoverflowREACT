ce4dea8126c295840cdb960819894c75c4c49dff
RJPinoy <rejenaligoradarsonval@gmail.com>
2024-02-09 11:20:41 +0100

    store peanut

--- stackoverflow-react/src/main.jsx
+++ stackoverflow-react/src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Store } from '@reduxjs/toolkit'
+// import { store } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={Store}>
+    {/* <Provider store={store}> */}
       <App />
-    </Provider>
+    {/* </Provider> */}
   </React.StrictMode>,
 )
